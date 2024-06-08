abcd5d529e10a5ead3fe34d70c3b14c176fc8322
kessyyolanda <kessyyolanda16@gmail.com>
2024-06-08 15:21:45 +0700

    menambahkan crud

--- projectkel5/database/migrations/2024_06_08_071743_create_product_tag_table.php
+++ projectkel5/database/migrations/2024_06_08_071743_create_product_tag_table.php
@@ -8,19 +8,25 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('product_tag', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('tag_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('product_tag');
     }
